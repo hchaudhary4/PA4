170f00d4ccff60f1a2565e262e2256bc89e184f5
Dletyaeva <123666979+Dletyaeva@users.noreply.github.com>
2023-04-08 01:17:15 +0000

    created const var, buffer for output file and checks for file names and prints input file to terminal

--- main.c
+++ main.c
@@ -11,10 +11,14 @@
  * Authors: Daleela Letyaeva
 */
 
-int *SPEED_CONSTANT[4] = {60, 70, 80};
 
 int main (int argc, char **argv){
 
+int const SIZE = 200; //arbitrary size used to read the lines from input file
+int const SPEED[4] = {60, 70, 80}; 
+char *Buffer = (char*)malloc(sizeof(char) * SIZE); // temp array to hold the informatino read from input file
+
+//checks for valid file names and input
 if(argc != 3){
    printf("Usage: enter input file name and output filename after %s\n", argv[0]);
    exit(1);
@@ -23,14 +27,28 @@ if(argc != 3){
 if(strcmp(argv[1],argv[2])== 0){
    printf("please enter different output file name.");
 }
+
+
    FILE *inputFile = fopen(argv[1], "r");
+   
+   if(inputFile == NULL){
+   printf("error reading file");
+   exit(1);
+   }
+
    FILE *outputFile = fopen(argv[2],"w");
+
+   //read inputfile and print the contents into output file
+   while(fgets(Buffer, SIZE, inputFile)){
+      fprintf(stdout, "%s", Buffer);
+   }
+   
 //read input file
 //testPrint(inputFile);
 
-
-fclose(argv[1]);
-fclose(argv[2]);
+//close files once done
+fclose(inputFile);
+fclose(outputFile);
 
 //don't forget to free your memory for arrays
 return 0;
