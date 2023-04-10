223ce4f7d3b130930908dfd911b58abab6de1667
Dletyaeva <123666979+Dletyaeva@users.noreply.github.com>
2023-04-10 03:56:06 +0000

    update 5

--- test.c
+++ test.c
@@ -11,54 +11,80 @@
 */
 typedef struct Car_stru {
     char carName[100];
-    float dragCo;
-    float dragArea;
-    float powerArr;
+    double dragCo;
+    double dragArea;
+    double powerArr[4];
 } Car;
 
 
 //function to process information by car
-void subdivideCars(char *argv[1], Car **carArray){
+void subdivideCarsFromInput(FILE *inputFile, Car *carNum, int const SIZE_ELE, int const SIZE_ARR, char *BUFFER){
+    int i = 0;
+    FILE *in = inputFile;
+    while(fgets(BUFFER, SIZE_ARR, in) != NULL){
+        //BUFFER[strlen(BUFFER) - 1] = "\0";
+        //
+        char *cname = strtok(BUFFER, ",");
+        strcpy(carNum[i].carName, cname);
+        char *cdragCO = strtok(NULL,",");
+        carNum[i].dragCo = atof(cdragCO);
+        char *dragarea = strtok(NULL,",");
+        carNum[i].dragArea = atof(dragarea);
+        }
 
-}
+    }
 
 
 
 void main (int argc, char **argv){
-    int SIZE = 300;
-    Car **carArray = (Car**) malloc (sizeof(Car*)*SIZE); // allocated memory for the general array
-    for(int i =0; i < SIZE; i++){
-        Car *carNum = (Car*) malloc (sizeof(Car) * SIZE); // allocated memory for each element in the array
-    }
     
+    //read input file
+    FILE *inputFile = fopen(argv[1], "r");
 
-    // read input file
-   FILE *inputFile = fopen(argv[1], "r");
-   
-   if(inputFile == NULL){
-   printf("error reading file");
-   exit(1);
-   }
-
-    //process file
-    // function that that creates a variable for each car
-    //and stores those variables in a array
-
-    char *Buffer = (char*)malloc(sizeof(char) * SIZE);
-    
-    
-    for(int i = 0; i < SIZE; i++ ){
-        Car *carNum = (carNum*)malloc(sizeof(carNum)*SIZE);
+    //check that user enters in/out file name
+    if(argc != 3){
+    printf("Usage: enter input file name and output filename after %s\n", argv[0]);
+    exit(1);
     }
 
+    //try to avoid user from confusing the 2 files
+    if(strcmp(argv[1],argv[2])== 0){
+    printf("please enter different output file name.");
+    }
+
+   //check for file not to be empty
+    if(inputFile == NULL){
+     printf("error reading file");
+     exit(1);
+    }
+
+    int const SIZE_ARR = 300;
+    int const SIZE_ELE = 100;
+
+    char *BUFFER= (char*)malloc(sizeof(char) * SIZE_ELE);
+
+    Car **carArray = (Car**) malloc (sizeof(Car*) * SIZE_ARR); // allocated memory for the general array
+    Car *carNum;
+    for(int i =0; i < SIZE_ARR; i++){
+        carNum = (Car*) malloc (sizeof(Car) * SIZE_ELE); // allocated memory for each element in the general array
+    }
 
 
-
-    //print output file
+    
+    //open output file
     FILE *outputFile = fopen(argv[2],"w");
+    
+    //process file
+    //read input file line by line
+    //and catagorize the information
+    subdivideCarsFromInput(inputFile, carNum, SIZE_ELE, SIZE_ARR, BUFFER);
+    
+    for(int k = 0; k < SIZE_ARR; k++){
+    fprintf(inputFile,"%s %.2lf %.2lf", carNum[k].carName,carNum[k].dragCo, carNum[k].dragArea);
+    }
+        
+    //close the input file
+    fclose(inputFile);
 
-
-fclose(inputFile);
-free(Buffer);
 return;
 }
