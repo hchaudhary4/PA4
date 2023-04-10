e587334aa769c7f2114179871e2edaf1c0cdae89
Dletyaeva <123666979+Dletyaeva@users.noreply.github.com>
2023-04-10 01:33:57 -0500

    mainV2.c
    allows user to enter an input file, it takes that file and reads it line by line  storing anything in front of a comma in an array. Then it sorts out the data, specifically takes the original array and "deletes" entries that have negative drag area and stores the positive entries in a new array.

--- main.c
+++ main.c
@@ -5,63 +5,136 @@
 #include <math.h>
 #include <string.h>
 
- //when compiling this file compile as usual
 
 /**
  * Authors: Daleela Letyaeva
 */
+typedef struct Car_stru {
+    char carName[100];
+    double dragCo;
+    double dragArea;
+    double powerArr[4];
+} Car;
 
-
-int main (int argc, char **argv){
-
-int const SIZE = 200; //arbitrary size used to read the lines from input file
-int const SPEED[4] = {60, 70, 80}; 
-char *Buffer = (char*)malloc(sizeof(char) * SIZE); // temp array to hold the informatino read from input file
-
-//checks for valid file names and input
-if(argc != 3){
-   printf("Usage: enter input file name and output filename after %s\n", argv[0]);
-   exit(1);
+//pre-reads the input file to figure out how many rows it has ==> how big general array needs to be
+int getFileRows(char *BUFFER, FILE *inputFile){
+int i = 0;
+while(fgets(BUFFER, 200, inputFile) != NULL){
+    i++;
 }
-
-if(strcmp(argv[1],argv[2])== 0){
-   printf("please enter different output file name.");
-}
-
-   FILE *inputFile = fopen(argv[1], "r");
-   
-   if(inputFile == NULL){
-   printf("error reading file");
-   exit(1);
-   }
-
-   FILE *outputFile = fopen(argv[2],"w");
-
-   //read inputfile and print the contents into output file
-   //currently it's reading the contents of input file and copying them into output file
-   printf("writing output file\n");
-   while(fgets(Buffer, SIZE, inputFile) != NULL){
-      fputs(Buffer, outputFile);
-   }
-   printf("finished writing output file\n");
-   
-//read input file
-//testPrint(inputFile);
-
-   FILE *outputFile = fopen(argv[2],"w");
-
-   //read inputfile and print the contents into output file
-   while(fgets(Buffer, SIZE, inputFile)){
-      fprintf(stdout, "%s", Buffer);
-   }
-   
-//read input file
-//testPrint(inputFile);
-
-//close files once done
 fclose(inputFile);
-fclose(outputFile);
+return i;
+}
+//function to process information by car
+void subdivideCarsFromInput(char const *inFILE, Car **carArray, int const SIZE_ELE, int const SIZE_ARR, char *BUFFER){
+    FILE *inputFile = fopen(inFILE, "r");
 
-//don't forget to free your memory for arrays
-return 0;
+    if(inputFile == NULL){
+     printf("error reading file\n");
+     exit(1);
+    }
+    int i = 0;
+    while(fgets(BUFFER, SIZE_ELE, inputFile) != NULL){
+       // BUFFER[strlen(BUFFER)-1] = '\0';
+        char *cname = strtok(BUFFER, ",");
+        strcpy(carArray[i]->carName, cname);
+        char *cdragCO = strtok(NULL,",");
+        carArray[i]->dragCo = atof(cdragCO);
+        char *dragarea = strtok(NULL,"\n");
+        carArray[i]->dragArea = atof(dragarea);
+        i++;
+        }
+
+    fclose(inputFile);
+    }
+
+    void sortArray (Car **carArray, int const SIZE_ARR, int const SIZE_ELE){
+       int count = 0;
+        for(int i = 0; i < SIZE_ARR; i++){
+            if(carArray[i]->dragArea == -1){
+            count++;
+            }
+        }
+
+        Car **sortedArray = (Car**)malloc(sizeof(Car*) * count);
+        for(int j = 0; j < count; j++){
+            sortedArray[j] = (Car*)malloc(sizeof(Car) * SIZE_ELE);
+        }
+       
+        fprintf(stdout, "*******CARS W/ POSITIVE DRAG AREA*******\n");
+        for(int k = 0; k < SIZE_ARR; k++){
+            if(carArray[k]->dragArea != -1){
+                sortedArray[k] = carArray[k];
+            } else {
+                continue;
+            }
+        fprintf(stdout, "%s\n", sortedArray[k] ->carName);
+        }
+    }
+
+
+void main (int argc, char **argv){
+
+    //check that user enters in/out file name
+    if(argc != 2){ //FIXME LATER
+    printf("Usage: enter input file name and output filename after %s\n", argv[0]);
+    exit(1);
+    }
+
+    //try to avoid user from confusing the 2 files
+    /*
+    if(strcmp(argv[1],argv[2])== 0){
+    printf("please enter different output file name.");
+    }
+    */
+    
+    //read input file
+    char const *inFILE = argv[1];
+    FILE *inputFile = fopen(inFILE, "r");
+    
+
+   //check for file not to be empty
+    if(inputFile == NULL){
+     printf("error reading file");
+     exit(1);
+    }
+
+    
+    int const SIZE_ELE = 100;
+    char *BUFFER= (char*)malloc(sizeof(char) * SIZE_ELE);
+
+    int const SIZE_ARR = getFileRows(BUFFER, inputFile);
+    printf("# of file rows = %d\n", SIZE_ARR);
+    
+    //close the input file
+    
+
+    Car **carArray = (Car**) malloc (sizeof(Car*) * SIZE_ARR); // allocated memory for the general array
+    for(int i =0; i < SIZE_ARR; i++){
+        carArray[i] = (Car*) malloc (sizeof(Car) * SIZE_ELE); // allocated memory for each element in the general array
+    }
+
+
+    
+
+    
+    //process file
+    //read input file line by line and store info in seperately
+    subdivideCarsFromInput(inFILE, carArray, SIZE_ELE, SIZE_ARR, BUFFER);
+    sortArray(carArray, SIZE_ARR, SIZE_ELE);
+
+    //open output file
+    //FILE *outputFile = fopen(argv[2],"w");
+    /*
+    for(int k = 0; k < SIZE_ARR; k++){
+    fprintf(stdout,"row[%d]: %s %.2lf %.2lf\n", k, carArray[k]->carName,carArray[k]->dragCo,carArray[k]->dragArea);
+    }
+    */
+        
+
+
+free(BUFFER);
+free(carArray);
+
+return;
 }
