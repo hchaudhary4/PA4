66e4b9e08dbc2650097f275b363ce2a944734ac5
Dletyaeva <123666979+Dletyaeva@users.noreply.github.com>
2023-04-11 02:14:16 -0500

    Update definition.h

--- definition.h
+++ definition.h
@@ -3,29 +3,4 @@
 #ifndef DEFINITION_H
 #define DEFINITION_H
 
-/*
-*this struct stores the data associated with the car
-*/
-/*
-typedef struct Car_struct{
-   char cName[100];
-   double dragCo;
-   double dragArea; 
-   double powerArr[4]; 
-} Car;
-
-*/
-
-
-/*
-*this struct stores the data associated with the car
-*/
-struct Car{
-   char cName[100];
-   double dragCo;
-   double dragArea; 
-   double powerArr[4];
-   int speed[4];   
-} block;
-
 #endif
