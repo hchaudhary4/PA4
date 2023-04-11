d4ae0f857fd4584e50fcb6b22452caaf84794757
Dletyaeva <123666979+Dletyaeva@users.noreply.github.com>
2023-04-11 06:20:03 +0000

    have the functions in seperate files main.c is updated from test.c main.c prints cars with positive drag area in terminal and in output file

--- main.c
+++ main.c
@@ -5,57 +5,80 @@
 #include <math.h>
 #include <string.h>
 
- //when compiling this file compile as usual
-
 /**
  * Authors: Daleela Letyaeva
 */
 
-
 void main (int argc, char **argv){
 
-int const SIZE = 200; //arbitrary size used to read the lines from input file
-int const SPEED[4] = {60, 70, 80}; 
-char *Buffer = (char*)malloc(sizeof(char) * SIZE); // temp array to hold the informatino read from input file
-Car *cBlockArr = (Car*)malloc(sizeof(Car)* SIZE);
+    //check that user enters in/out file name
+    if(argc != 3){ //FIXME LATER
+    printf("Usage: enter input file name and output filename after %s\n", argv[0]);
+    exit(1);
+    }
 
-//checks for valid file names and inputs
-if(argc != 3){
-   printf("Usage: enter input file name and output filename after %s\n", argv[0]);
-   exit(1);
-}
+    //try to avoid user from confusing the 2 files
+    if(strcmp(argv[1],argv[2])== 0){
+    printf("please enter different output file name.");
+    }
+    
+    //read input file
+    char const *inFILE = argv[1];
+    FILE *inputFile = fopen(inFILE, "r");
+    
 
-if(strcmp(argv[1],argv[2])== 0){
-   printf("please enter different output file name.");
-}
+   //check for file not to be empty
+    if(inputFile == NULL){
+     printf("error reading file");
+     exit(1);
+    }
+
+    //variable definitions 
+    int const SIZE_ELE = 100;
+    char *BUFFER= (char*)malloc(sizeof(char) * SIZE_ELE);
+
+    int const SIZE_ARR = getFileRows(BUFFER, inputFile);
+    printf("# of file rows = %d\n", SIZE_ARR);
+    
+    Car **carArray = (Car**) malloc (sizeof(Car*) * SIZE_ARR); // allocated memory for the general array
+        for(int i =0; i < SIZE_ARR; i++){
+            carArray[i] = (Car*) malloc (sizeof(Car) * SIZE_ELE); // allocated memory for each element in the general array
+        }
+
+    //opens file in funciton
+    //process file by read input file line by line and store info in seperately
+    //closes file once done reading
+    subdivideCarsFromInput(inFILE, carArray, SIZE_ELE, SIZE_ARR, BUFFER);
+
+    //this variable is the value size for new array
+    int count = countPositive(carArray, SIZE_ARR, SIZE_ELE); 
+
+    Car **sortedArray = (Car**)malloc(sizeof(Car*) * count);
+        for(int j = 0; j < count; j++){
+            sortedArray[j] = (Car*)malloc(sizeof(Car) * SIZE_ELE);
+        }
+
+    //sort the old array into new one
+    //put into a function later
+    eliminateCars(count, SIZE_ELE, SIZE_ARR, carArray, sortedArray);
+    
+    //open output file
+    FILE *outputFile = fopen(argv[2],"w");
+
+    //writes the data from Array to txt file
+    printToOutputFile(sortedArray, count, outputFile);
+
+    //function that computes power
+    //computePower();
+
+    //function that compares the data of two cars 
+    //carCmp();
 
 
-
-
-   //read inputfile and print the contents into output file
-   //currently it's reading the contents of input file and copying them into output file
-   printf("writing output file\n");
-   int i = 0;
-   while(fgets(Buffer, SIZE, inputFile) != NULL){
-     // fputs(Buffer, outputFile); FIXME
-     cBlockArr[i].cName = strtok(Buffer, ",");
-     cBlockArr[i].dragCo = atof(strtok(NULL, ","));
-     cBlockArr[i].dragArea = atof(strtok(NULL, ","));
-     i++;
-
-   }
-    //print
-   printf("finished writing output file\n");
-   for(int j =0; j < strlen(cBlockArr); j++){
-      fprintf(outputFile, "%s, %lf, %lf", cBlockArr[i].cName,cBlockArr[i].dragCo, cBlockArr[i].dragArea);
-     }
-//read input file
-//testPrint(inputFile);
-
-//close files once done
-fclose(inputFile);
 fclose(outputFile);
+free(BUFFER);
+free(carArray);
+free(sortedArray);
 
-//don't forget to free your memory for arrays
-return 0;
+return;
 }
